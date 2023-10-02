5732a439dc895eec531be6045134ebdbc16bb0c5
akorton <akortysh@yandex.ru>
2023-10-02 08:25:46 +0300

    Should be easer with protege

--- prolog/abilities.pl
+++ prolog/abilities.pl
@@ -1,78 +1,108 @@
-:- module(abilities, [ability/2, ability_type/2, hero_ability_type/2, hero_with_stun/1, hero_with_dmg/1, universal_hero/1]).
+:- module(abilities, [hero_ability/2, ability_type/2, hero_ability_type/2, hero_with_stun/1, hero_with_dmg/1, universal_hero/1]).
 :- use_module(heroes).
 
+% ability(ability_name)
+ability("BERSERKER'S CALL").
+ability("FIEND'S GRIP").
+ability("FLAMING LASSO").
+ability("PRIMAL ROAR").
+ability("FREEZING FIELD").
+ability("VACUUM").
+ability("BEDLAM").
+ability("TERRORIZE").
+ability("MAGNETIZE").
+ability("ECHO SLAM").
+ability("ECHO STOMP").
+ability("EARTH SPLITTER").
+ability("IMPETUS").
+ability("BLACK HOLE").
+ability("CHRONOSPHERE").
+ability("CALL DOWN").
+ability("SHARPSHOOTER").
+ability("LIFE BREAK").
+ability("LIFE BREAK AGHANIM'S SCEPTER").
+ability("SUN STRIKE").
+ability("SUN STRIKE AGHANIM'S SCEPTER").
+ability("ICE PATH").
+ability("MACROPYRE").
+ability("OMNISLASH").
+ability("DUEL").
+ability("LAGUNA BLADE").
+ability("FINGER OF DEATH").
+ability("REVERSE POLARITY").
+ability("WUKONG'S COMMAND").
 
-% ability(ability_name, hero_name)
-ability("BERSERKER'S CALL", "AXE").
-ability("FIEND'S GRIP", "BANE").
-ability("FLAMING LASSO", "BATRIDER").
-ability("PRIMAL ROAR", "BEASTMASTER").
-ability("FREEZING FIELD", "CRYSTAL MAIDEN").
-ability("VACUUM", "DARK SEER").
-ability("BEDLAM", "DARK WILLOW").
-ability("TERRORIZE", "DARK WILLOW").
-ability("MAGNETIZE", "EARTH SPIRIT").
-ability("ECHO SLAM", "EARTHSHAKER").
-ability("ECHO STOMP", "ELDER TITAN").
-ability("EARTH SPLITTER", "ELDER TITAN").
-ability("IMPETUS", "ENCHANTRESS").
-ability("BLACK HOLE", "ENIGMA").
-ability("CHRONOSPHERE", "FACELESS VOID").
-ability("CALL DOWN", "GYROCOPTER").
-ability("SHARPSHOOTER", "HOODWINK").
-ability("LIFE BREAK", "HUSKAR").
-ability("LIFE BREAK AGHANIM'S SCEPTER", "HUSKAR").
-ability("SUN STRIKE", "INVOKER").
-ability("SUN STRIKE AGHANIM'S SCEPTER", "INVOKER").
-ability("ICE PATH", "JAKIRO").
-ability("MACROPYRE", "JAKIRO").
-ability("OMNISLASH", "JUGGERNAUT").
-ability("DUEL", "LEGION COMMANDER").
-ability("LAGUNA BLADE", "LINA").
-ability("FINGER OF DEATH", "LION").
-ability("REVERSE POLARITY", "MAGNUS").
-ability("WUKONG'S COMMAND", "MONKEY KING").
+% hero_ability(ability_name, hero_name)
+hero_ability(ability("BERSERKER'S CALL"), hero("AXE")).
+hero_ability(ability("FIEND'S GRIP"), hero("BANE")).
+hero_ability(ability("FLAMING LASSO"), hero("BATRIDER")).
+hero_ability(ability("PRIMAL ROAR"), hero("BEASTMASTER")).
+hero_ability(ability("FREEZING FIELD"), hero("CRYSTAL MAIDEN")).
+hero_ability(ability("VACUUM"), hero("DARK SEER")).
+hero_ability(ability("BEDLAM"), hero("DARK WILLOW")).
+hero_ability(ability("TERRORIZE"), hero("DARK WILLOW")).
+hero_ability(ability("MAGNETIZE"), hero("EARTH SPIRIT")).
+hero_ability(ability("ECHO SLAM"), hero("EARTHSHAKER")).
+hero_ability(ability("ECHO STOMP"), hero("ELDER TITAN")).
+hero_ability(ability("EARTH SPLITTER"), hero("ELDER TITAN")).
+hero_ability(ability("IMPETUS"), hero("ENCHANTRESS")).
+hero_ability(ability("BLACK HOLE"), hero("ENIGMA")).
+hero_ability(ability("CHRONOSPHERE"), hero("FACELESS VOID")).
+hero_ability(ability("CALL DOWN"), hero("GYROCOPTER")).
+hero_ability(ability("SHARPSHOOTER"), hero("HOODWINK")).
+hero_ability(ability("LIFE BREAK"), hero("HUSKAR")).
+hero_ability(ability("LIFE BREAK AGHANIM'S SCEPTER"), hero("HUSKAR")).
+hero_ability(ability("SUN STRIKE"), hero("INVOKER")).
+hero_ability(ability("SUN STRIKE AGHANIM'S SCEPTER"), hero("INVOKER")).
+hero_ability(ability("ICE PATH"), hero("JAKIRO")).
+hero_ability(ability("MACROPYRE"), hero("JAKIRO")).
+hero_ability(ability("OMNISLASH"), hero("JUGGERNAUT")).
+hero_ability(ability("DUEL"), hero("LEGION COMMANDER")).
+hero_ability(ability("LAGUNA BLADE"), hero("LINA")).
+hero_ability(ability("FINGER OF DEATH"), hero("LION")).
+hero_ability(ability("REVERSE POLARITY"), hero("MAGNUS")).
+hero_ability(ability("WUKONG'S COMMAND"), hero("MONKEY KING")).
 
 % ability_type(ability_name, ability_type), where ability type is one of [dmg_single, dmg_aoe, stun_single, stun_aoe]
 % dmg_single - huge damage to solo target
 % dmg_aoe - huge damage to big area
 % stun_single - great control to one target
 % stun_single - great control to big area
-ability_type("BERSERKER'S CALL", stun_aoe).
-ability_type("FIEND'S GRIP", stun_single).
-ability_type("FLAMING LASSO", stun_single).
-ability_type("PRIMAL ROAR", stun_single).
-ability_type("FREEZING FIELD", dmg_aoe).
-ability_type("VACUUM", stun_aoe).
-ability_type("BEDLAM", dmg_single).
-ability_type("TERRORIZE", stun_aoe).
-ability_type("MAGNETIZE", dmg_aoe).
-ability_type("ECHO SLAM", stun_aoe).
-ability_type("ECHO SLAM", dmg_aoe).
-ability_type("ECHO STOMP", stun_aoe).
-ability_type("EARTH SPLITTER", dmg_aoe).
-ability_type("IMPETUS", dmg_single).
-ability_type("BLACK HOLE", stun_aoe).
-ability_type("BLACK HOLE", dmg_aoe).
-ability_type("CHRONOSPHERE", stun_aoe).
-ability_type("CALL DOWN", dmg_aoe).
-ability_type("SHARPSHOOTER", dmg_single).
-ability_type("LIFE BREAK", dmg_single).
-ability_type("LIFE BREAK AGHANIM'S SCEPTER", stun_single).
-ability_type("SUN STRIKE", dmg_single).
-ability_type("SUN STRIKE AGHANIM'S SCEPTER", dmg_aoe).
-ability_type("ICE PATH", stun_aoe).
-ability_type("MACROPYRE", dmg_aoe).
-ability_type("OMNISLASH", dmg_aoe).
-ability_type("OMNISLASH", dmg_single).
-ability_type("DUEL", stun_single).
-ability_type("LAGUNA BLADE", dmg_single).
-ability_type("FINGER OF DEATH", dmg_single).
-ability_type("REVERSE POLARITY", stun_aoe).
-ability_type("WUKONG'S COMMAND", dmg_aoe).
+ability_type(ability("BERSERKER'S CALL"), stun_aoe).
+ability_type(ability("FIEND'S GRIP"), stun_single).
+ability_type(ability("FLAMING LASSO"), stun_single).
+ability_type(ability("PRIMAL ROAR"), stun_single).
+ability_type(ability("FREEZING FIELD"), dmg_aoe).
+ability_type(ability("VACUUM"), stun_aoe).
+ability_type(ability("BEDLAM"), dmg_single).
+ability_type(ability("TERRORIZE"), stun_aoe).
+ability_type(ability("MAGNETIZE"), dmg_aoe).
+ability_type(ability("ECHO SLAM"), stun_aoe).
+ability_type(ability("ECHO SLAM"), dmg_aoe).
+ability_type(ability("ECHO STOMP"), stun_aoe).
+ability_type(ability("EARTH SPLITTER"), dmg_aoe).
+ability_type(ability("IMPETUS"), dmg_single).
+ability_type(ability("BLACK HOLE"), stun_aoe).
+ability_type(ability("BLACK HOLE"), dmg_aoe).
+ability_type(ability("CHRONOSPHERE"), stun_aoe).
+ability_type(ability("CALL DOWN"), dmg_aoe).
+ability_type(ability("SHARPSHOOTER"), dmg_single).
+ability_type(ability("LIFE BREAK"), dmg_single).
+ability_type(ability("LIFE BREAK AGHANIM'S SCEPTER"), stun_single).
+ability_type(ability("SUN STRIKE"), dmg_single).
+ability_type(ability("SUN STRIKE AGHANIM'S SCEPTER"), dmg_aoe).
+ability_type(ability("ICE PATH"), stun_aoe).
+ability_type(ability("MACROPYRE"), dmg_aoe).
+ability_type(ability("OMNISLASH"), dmg_aoe).
+ability_type(ability("OMNISLASH"), dmg_single).
+ability_type(ability("DUEL"), stun_single).
+ability_type(ability("LAGUNA BLADE"), dmg_single).
+ability_type(ability("FINGER OF DEATH"), dmg_single).
+ability_type(ability("REVERSE POLARITY"), stun_aoe).
+ability_type(ability("WUKONG'S COMMAND"), dmg_aoe).
 
-% Get hero ability types
-hero_ability_type(HERO, TYPE) :- hero(HERO), ability(ABILITY, HERO), ability_type(ABILITY, TYPE).
+% Get hero hero_ability types
+hero_ability_type(HERO, TYPE) :- hero_ability(ability(ABILITY), hero(HERO)), ability_type(ability(ABILITY), TYPE).
 hero_with_stun(HERO) :- hero_ability_type(HERO, TYPE), (TYPE = stun_single; TYPE = stun_aoe).
 hero_with_dmg(HERO) :- hero_ability_type(HERO, TYPE), (TYPE = dmg_single; TYPE = dmg_aoe).
 % Hero with both stun skill and dmg skill
--- prolog/main.pl
+++ prolog/main.pl
@@ -9,7 +9,8 @@ print_test(Test, Expected_result, Custom_test_name) :- ((Test, Expected_result);
     ansi_format([bold, fg(green)], 'Test: ~w passed\n', [Custom_test_name]).
 print_test(Test, Expected_result, Custom_test_name) :- ansi_format([bold, fg(red)], 'Test: ~w failed\n', [Custom_test_name]).
 
-?- writeln("Synergy tests.").
+?- writeln("").
+?- writeln("Synergy/2 tests.").
 ?- print_test(heroes_synergy("AXE", "INVOKER"), true). % stun_aoe, dmg_aoe
 ?- print_test(heroes_synergy("INVOKER", "AXE"), true). % dmg_aoe, stun_aoe
 ?- print_test(heroes_synergy("BANE", "INVOKER"), true). % stun_single, dmg_single
@@ -24,11 +25,15 @@ print_test(Test, Expected_result, Custom_test_name) :- ansi_format([bold, fg(red
 ?- print_test(heroes_synergy("DARK WILLOW", "FACELESS VOID"), false). % stun_aoe, stun_aoe
 ?- print_test(heroes_synergy("EARTHSHAKER", "EARTHSHAKER"), false). % Can't be synergy with itself
 ?- print_test(heroes_synergy("HUSKAR", "HUSKAR"), false). % Can't be synergy with itself
+?- writeln("").
+?- writeln("Synergy/3 tests.").
 ?- print_test(heroes_synergy("ENIGMA", "DARK SEER", "JAKIRO"), true). % Some synergy/3 tests
 ?- print_test(heroes_synergy("ELDER TITAN", "MAGNUS", "CRYSTAL MAIDEN"), true). % Some synergy/3 tests
 ?- print_test(heroes_synergy("EARTHSHAKER", "ELDER TITAN", "GYROCOPTER"), true). % Some synergy/3 tests
 ?- print_test(heroes_synergy("DARK WILLOW", "BANE", "HUSKAR"), true). % Some synergy/3 tests
 % Check for synergy with specific hero which should have stun, dmg ability or both
+?- writeln("").
+?- writeln("Complex synergy tests tests.").
 ?- print_test((findall(H, (hero(H), heroes_synergy(H, "INVOKER"), universal_hero(H)), L), member("HUSKAR", L)), true, "Universal hero synergy with INVOKER").
 ?- print_test((findall(H, (hero(H), heroes_synergy(H, "GYROCOPTER"), universal_hero(H)), L), member("ENIGMA", L)), true, "Universal hero synergy with GYROCOPTER").
 ?- print_test((findall(H, (hero(H), heroes_synergy(H, "BANE"), hero_with_stun(H)), L), member("DARK WILLOW", L)), true, "Hero with stun synergy with BANE").
